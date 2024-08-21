eda7739cf5a5686b504ca0491592ba03487666c9
kjmj <kmorton15@gmail.com>
2024-08-21 17:09:28 -0600

    wrap app in SafeAreaView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,24 @@
 import store from "@/store/store";
 import { Stack } from "expo-router";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { Provider } from 'react-redux';
 
 export default function RootLayout() {
   return (
-    <Provider store={store}>
-      <Stack>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="PostDetailView" />
-      </Stack>
-    </Provider>
+    <SafeAreaView style={styles.container}>
+      <Provider store={store}>
+        <Stack>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="PostDetailView" />
+        </Stack>
+      </Provider>
+    </SafeAreaView>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
