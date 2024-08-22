55bbb7919388087a8127a426086287f86c9a038e
kjmj <kmorton15@gmail.com>
2024-08-21 20:42:49 -0600

    show a cleaner custom header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,30 @@
-import store from "@/store/store";
-import { Stack } from "expo-router";
-import { SafeAreaView, StyleSheet } from "react-native";
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
+import { Stack } from 'expo-router';
+import store from '@/store/store';
+import CustomHeader from '@/components/CustomHeader';
 
 export default function RootLayout() {
   return (
     <SafeAreaView style={styles.container}>
       <Provider store={store}>
         <Stack>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="PostDetailView" />
+          <Stack.Screen
+            name="index"
+            options={{
+              header: () => <CustomHeader title="Home" />,
+            }}
+          />
+          <Stack.Screen
+            name="PostDetailView"
+            options={{
+              header: () => <CustomHeader showBackButton={true} />,
+            }}
+          />
         </Stack>
       </Provider>
     </SafeAreaView>
-
   );
 }
 
