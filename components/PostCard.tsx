1b87da6abcfd575ef099511263029a1ea5c1dadb
kjmj <kmorton15@gmail.com>
2024-08-21 18:43:56 -0600

    dont show more than 3 lines of text. add ...see more button to show all text

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,33 +1,48 @@
-import { CommentsMapType } from '@/types/CommentsMapType';
-import { Post } from '@/types/Post';
-import { router } from 'expo-router';
-import { View, Text, StyleSheet, Button, TouchableWithoutFeedback } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { ThemedText } from './ThemedText';
 import IconButton from '@/components/IconButton';
 import { updatePostField } from '@/store/postsSlice';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '@/store/store';
+import { CommentsMapType } from '@/types/CommentsMapType';
+import { Post } from '@/types/Post';
+import { router } from 'expo-router';
 
 interface PostCardProps {
     post: Post;
     disableTapOnPost: boolean;
+    showMore: boolean;
 }
 
-export function PostCard({ post, disableTapOnPost }: PostCardProps) {
+export function PostCard({ post, disableTapOnPost, showMore }: PostCardProps) {
     const dispatch: AppDispatch = useDispatch();
+    const [seeMore, setSeeMore] = useState(showMore);
 
     const handleHeartPress = () => {
         dispatch(updatePostField({ post: post, field: 'num_hugs', value: post.num_hugs + 1 }));
     };
 
+    const toggleSeeMore = () => {
+        setSeeMore(!seeMore);
+    };
+
     return (
         <View style={styles.container}>
             <TouchableWithoutFeedback onPress={disableTapOnPost ? () => { } : () => postPress(post)}>
-                <View style={styles.zzz}>
+                <View style={styles.bottomBorder}>
                     <ThemedText type='defaultSemiBold' style={styles.textContainer}>{post.title}</ThemedText>
-                    <Text style={styles.textContainer}>{post.patient_description}</Text>
+                    <Text style={styles.textContainer} numberOfLines={seeMore ? undefined : 3}>
+                        {post.patient_description}
+                    </Text>
+                    {!seeMore && (
+                        <TouchableWithoutFeedback onPress={toggleSeeMore}>
+                            <Text style={styles.seeMoreText}>...see more</Text>
+                        </TouchableWithoutFeedback>
+                    )}
                 </View>
-            </TouchableWithoutFeedback>
+            </TouchableWithoutFeedback >
+
             <View style={styles.engagementContainer}>
                 <IconButton
                     iconName="heart"
@@ -53,15 +68,13 @@ export function PostCard({ post, disableTapOnPost }: PostCardProps) {
                     filledColor="blue"
                     showCount={false}
                 />
-
             </View>
-        </View>
-    )
+        </View >
+    );
 }
 
-
 function numComments(comments: CommentsMapType): number {
-    return Object.keys(comments).length
+    return Object.keys(comments).length;
 }
 
 const styles = StyleSheet.create({
@@ -71,27 +84,26 @@ const styles = StyleSheet.create({
         padding: 15,
         borderRadius: 10,
         backgroundColor: "white",
-        // // Shadow for iOS
-        // shadowColor: '#000',
-        // shadowOffset: { width: 0, height: 2 },
-        // shadowOpacity: 0.3,
-        // shadowRadius: 4,
-        // // Shadow for Android
-        // elevation: 5,
     },
-    zzz: {
+    bottomBorder: {
         borderBottomColor: 'black',
         borderBottomWidth: StyleSheet.hairlineWidth,
         marginBottom: 6,
-
     },
     textContainer: {
         marginBottom: 12,
     },
+    seeMoreText: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        color: 'gray',
+        marginBottom: 12,
+        fontWeight: '700',
+    },
     engagementContainer: {
         flexDirection: "row",
-        justifyContent: "space-between"
-    }
+        justifyContent: "space-between",
+    },
 });
 
 function postPress(post: Post): void {
