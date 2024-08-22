f5567c54786ea07098be2beb668a5725c2edd188
kjmj <kmorton15@gmail.com>
2024-08-21 19:14:55 -0600

    add image to empty state

--- components/CommentList/EmptyState.tsx
+++ components/CommentList/EmptyState.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const EmptyState: React.FC = () => {
     return (
         <View style={styles.container}>
+            <Image
+                source={require('@/assets/images/conversation.png')}
+                style={styles.image}
+            />
             <Text style={styles.message}>No comments yet. Be the first to comment!</Text>
         </View>
     );
@@ -16,9 +20,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 20,
     },
+    image: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        marginBottom: 20,
+    },
     message: {
         fontSize: 16,
         color: 'gray',
+        textAlign: 'center', // Center-align text
     },
 });
 
