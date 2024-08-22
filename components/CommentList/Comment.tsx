8d4d93cd575bbb7785ff7c8e987ce5e9ac54874f
kjmj <kmorton15@gmail.com>
2024-08-21 19:02:15 -0600

    similar ux (avatar and time posted ago) for comments

--- components/CommentList/Comment.tsx
+++ components/CommentList/Comment.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { CommentType } from "@/components/CommentList/types/CommentType";
+import { getTimeAgo } from '@/util/utils';
 
 interface CommentProps {
     comment: CommentType;
@@ -9,9 +10,17 @@ interface CommentProps {
 export function Comment({ comment }: CommentProps) {
     return (
         <View style={styles.commentContainer}>
-            <Text style={styles.commentAuthor}>{comment.display_name}</Text>
+            <View style={styles.headerContainer}>
+                <Image
+                    source={{ uri: 'https://avatar.iran.liara.run/public' }} // Placeholder avatar URL
+                    style={styles.avatar}
+                />
+                <View style={styles.textContainer}>
+                    <Text style={styles.commentAuthor}>{comment.display_name}</Text>
+                    <Text style={styles.commentDate}>{getTimeAgo(new Date(comment.created_at))}</Text>
+                </View>
+            </View>
             <Text style={styles.commentText}>{comment.text}</Text>
-            <Text style={styles.commentDate}>{new Date(comment.created_at).toLocaleString()}</Text>
 
             {comment.children.length > 0 && (
                 <View style={styles.repliesContainer}>
@@ -22,27 +31,41 @@ export function Comment({ comment }: CommentProps) {
             )}
         </View>
     );
-};
+}
 
 const styles = StyleSheet.create({
     commentContainer: {
         padding: 10,
         marginLeft: 10,
     },
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
+    },
+    avatar: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        marginRight: 10,
+    },
+    textContainer: {
+        flex: 1,
+    },
     commentAuthor: {
         fontWeight: 'bold',
-    },
-    commentText: {
-        marginVertical: 5,
     },
     commentDate: {
         fontSize: 12,
         color: 'gray',
     },
+    commentText: {
+        marginVertical: 5,
+    },
     repliesContainer: {
-        marginLeft: 20,
+        marginLeft: 15,
         borderLeftWidth: 1,
         borderLeftColor: '#ccc',
-        paddingLeft: 10,
+        paddingLeft: 5,
     },
 });
